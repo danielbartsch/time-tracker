dfe95fd6b43ba73cc55205e994d42a271fe427bf
danielbartsch <daniel.bartsch@adverity.com>
2020-04-05 15:16:04 +0200

    applied prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,26 @@
-import React from "react";
-import { workDays, weekendDays, holidays, weekDays, birthdays } from "./config";
-import { addDays } from "./date";
+import React from 'react'
+import { workDays, weekendDays, holidays, weekDays, birthdays } from './config'
+import { addDays } from './date'
 
 const getDateWithoutTime = (datetime: Date) =>
-  new Date(datetime.getFullYear(), datetime.getMonth(), datetime.getDate());
+  new Date(datetime.getFullYear(), datetime.getMonth(), datetime.getDate())
 
-const padNumber = (
-  number: number,
-  pad: number,
-  zeros: boolean = true
-): string => {
+const padNumber = (number: number, pad: number, zeros: boolean = true): string => {
   if (pad < 1) {
-    return String(number);
+    return String(number)
   }
-  const digits = Math.floor(Math.log10(number)) + 1;
+  const digits = Math.floor(Math.log10(number)) + 1
   return `${Array.from({ length: pad - digits })
-    .map(() => (zeros ? "0" : " "))
-    .join("")}${number}`;
-};
+    .map(() => (zeros ? '0' : ' '))
+    .join('')}${number}`
+}
 
 const Day = ({ value }: { value: Date }) => (
   <>
     {weekDays[value.getDay()]}. {padNumber(value.getFullYear(), 4)}-
     {padNumber(value.getMonth() + 1, 2)}-{padNumber(value.getDate(), 2)}
   </>
-);
+)
 
 const DayStyle = ({
   children,
@@ -32,65 +28,57 @@ const DayStyle = ({
   todaysHolidays,
   todaysBirthdays,
 }: {
-  children: React.ReactNode;
-  date: Date;
-  todaysHolidays: Array<string>;
-  todaysBirthdays: Array<[string, Date]>;
+  children: React.ReactNode
+  date: Date
+  todaysHolidays: Array<string>
+  todaysBirthdays: Array<[string, Date]>
 }) => {
-  let textColor = undefined;
+  let textColor = undefined
   if (todaysHolidays.length > 0) {
-    textColor = "green";
+    textColor = 'green'
   }
   if (todaysBirthdays.length > 0) {
-    textColor = "#fc0";
+    textColor = '#fc0'
   }
 
-  let tooltip = todaysHolidays.slice();
+  let tooltip = todaysHolidays.slice()
   if (tooltip.length > 0 && todaysBirthdays.length > 0) {
-    tooltip.push("und");
+    tooltip.push('und')
   }
   tooltip.push(
     ...todaysBirthdays.map(
-      ([name, birthday]) =>
-        `${name} (${date.getFullYear() - birthday.getFullYear()})`
+      ([name, birthday]) => `${name} (${date.getFullYear() - birthday.getFullYear()})`
     )
-  );
+  )
   return (
-    <code
-      title={tooltip.join(" ")}
-      style={textColor ? { color: textColor } : undefined}
-    >
+    <code title={tooltip.join(' ')} style={textColor ? { color: textColor } : undefined}>
       {children}
     </code>
-  );
-};
+  )
+}
 
 const App = () => {
-  const [startDay, setStartDay] = React.useState(
-    getDateWithoutTime(new Date())
-  );
-  const [shownDays, setDays] = React.useState(30);
+  const [startDay, setStartDay] = React.useState(getDateWithoutTime(new Date()))
+  const [shownDays, setDays] = React.useState(30)
 
   React.useEffect(() => {
-    return document.addEventListener("wheel", (event) => {
+    return document.addEventListener('wheel', event => {
       if (event.deltaY !== 0) {
-        setStartDay((previousStartDay) =>
-          addDays(previousStartDay, event.deltaY)
-        );
+        setStartDay(previousStartDay => addDays(previousStartDay, event.deltaY))
       }
       if (event.deltaX !== 0) {
-        setDays((prevDays) => {
+        setDays(prevDays => {
           if (prevDays + event.deltaX < 1) {
-            return 1;
+            return 1
           }
           if (prevDays + event.deltaX > 100) {
-            return 100;
+            return 100
           }
-          return prevDays + event.deltaX;
-        });
+          return prevDays + event.deltaX
+        })
       }
-    });
-  }, []);
+    })
+  }, [])
 
   return (
     <div className="App">
@@ -99,27 +87,28 @@ const App = () => {
           <tr>
             <th>Datum</th>
             <th>Feiertag</th>
+            <th>Geburtstag</th>
             <th>Arbeit</th>
             <th>Wochenende</th>
           </tr>
         </thead>
         <tbody>
           {Array.from({ length: shownDays }).map((_, index) => {
-            const date = addDays(startDay, index);
+            const date = addDays(startDay, index)
 
             const todaysHolidays = holidays
               .filter(([, isHoliday]) => isHoliday(date))
-              .map(([name]) => name);
+              .map(([name]) => name)
 
             const todaysBirthdays = birthdays.filter(
               ([, birthday]) =>
                 birthday.getMonth() === date.getMonth() &&
                 birthday.getDate() === date.getDate() &&
                 date.getFullYear() >= birthday.getFullYear()
-            );
+            )
 
-            const isWorkday = workDays.includes(date.getDay());
-            const isWeekend = weekendDays.includes(date.getDay());
+            const isWorkday = workDays.includes(date.getDay())
+            const isWeekend = weekendDays.includes(date.getDay())
             return (
               <tr key={`${date}`}>
                 <td className="date">
@@ -131,18 +120,26 @@ const App = () => {
                     <Day value={date} />
                   </DayStyle>
                 </td>
-                <td>{todaysHolidays.length > 0 ? "o" : undefined}</td>
+                <td>{todaysHolidays.length > 0 ? todaysHolidays.join(', ') : undefined}</td>
                 <td>
-                  {isWorkday && todaysHolidays.length === 0 ? "o" : undefined}
+                  {todaysBirthdays.length > 0
+                    ? todaysBirthdays
+                        .map(
+                          ([name, birthday]) =>
+                            `${name} (${date.getFullYear() - birthday.getFullYear()})`
+                        )
+                        .join(', ')
+                    : undefined}
                 </td>
-                <td>{isWeekend ? "o" : undefined}</td>
+                <td>{isWorkday && todaysHolidays.length === 0 ? 'o' : undefined}</td>
+                <td>{isWeekend ? 'o' : undefined}</td>
               </tr>
-            );
+            )
           })}
         </tbody>
       </table>
     </div>
-  );
-};
+  )
+}
 
-export default App;
+export default App
--- src/config.ts
+++ src/config.ts
@@ -1,29 +1,29 @@
 import { addDays, getEasterDate, isEqual } from './date'
 
-export const weekDays = ["So", "Mo", "Di", "Mi", "Do", "Fr", "Sa"]
+export const weekDays = ['So', 'Mo', 'Di', 'Mi', 'Do', 'Fr', 'Sa']
 
 export const workDays = [1, 2, 3, 4, 5]
 export const weekendDays = [6, 0]
 
-export const holidays: Array<[string, ((date: Date) => boolean)]> = [
+export const holidays: Array<[string, (date: Date) => boolean]> = [
     /* austrian holidays */
-    ['Neujahr', (date) => date.getMonth() === 0 && date.getDate() === 1],
-    ['Heilige Drei Könige', (date) => date.getMonth() === 0 && date.getDate() === 6],
-    ['Ostersonntag', (date) => isEqual(getEasterDate(date),date)],
-    ['Ostermontag', (date) => isEqual(addDays(getEasterDate(date), 1),date)],
-    ['Staatsfeiertag', (date) => date.getMonth() === 4 && date.getDate() === 1],
-    ['Christi Himmelfahrt', (date) => isEqual(addDays(getEasterDate(date), 39),date)],
-    ['Pfingstsonntag', (date) => isEqual(addDays(getEasterDate(date), 49),date)],
-    ['Pfingstmontag', (date) => isEqual(addDays(getEasterDate(date), 50),date)],
-    ['Fronleichnam', (date) => isEqual(addDays(getEasterDate(date), 60),date)],
-    ['Mariä Himmelfahrt', (date) => date.getMonth() === 7 && date.getDate() === 15],
-    ['Nationalfeiertag', (date) => date.getMonth() === 9 && date.getDate() === 26],
-    ['Allerheiligen', (date) => date.getMonth() === 10 && date.getDate() === 1],
-    ['Mariä Empfängnis', (date) => date.getMonth() === 11 && date.getDate() === 8],
-    ['Heiliger Abend', (date) => date.getMonth() === 11 && date.getDate() === 24],
-    ['Christtag', (date) => date.getMonth() === 11 && date.getDate() === 25],
-    ['Stefanitag', (date) => date.getMonth() === 11 && date.getDate() === 26],
-    ['Silvester', (date) => date.getMonth() === 11 && date.getDate() === 31],
+  ['Neujahr', date => date.getMonth() === 0 && date.getDate() === 1],
+  ['Heilige Drei Könige', date => date.getMonth() === 0 && date.getDate() === 6],
+  ['Ostersonntag', date => isEqual(getEasterDate(date), date)],
+  ['Ostermontag', date => isEqual(addDays(getEasterDate(date), 1), date)],
+  ['Staatsfeiertag', date => date.getMonth() === 4 && date.getDate() === 1],
+  ['Christi Himmelfahrt', date => isEqual(addDays(getEasterDate(date), 39), date)],
+  ['Pfingstsonntag', date => isEqual(addDays(getEasterDate(date), 49), date)],
+  ['Pfingstmontag', date => isEqual(addDays(getEasterDate(date), 50), date)],
+  ['Fronleichnam', date => isEqual(addDays(getEasterDate(date), 60), date)],
+  ['Mariä Himmelfahrt', date => date.getMonth() === 7 && date.getDate() === 15],
+  ['Nationalfeiertag', date => date.getMonth() === 9 && date.getDate() === 26],
+  ['Allerheiligen', date => date.getMonth() === 10 && date.getDate() === 1],
+  ['Mariä Empfängnis', date => date.getMonth() === 11 && date.getDate() === 8],
+  ['Heiliger Abend', date => date.getMonth() === 11 && date.getDate() === 24],
+  ['Christtag', date => date.getMonth() === 11 && date.getDate() === 25],
+  ['Stefanitag', date => date.getMonth() === 11 && date.getDate() === 26],
+  ['Silvester', date => date.getMonth() === 11 && date.getDate() === 31],
 ]
 
 export const birthdays: Array<[string, Date]> = [
