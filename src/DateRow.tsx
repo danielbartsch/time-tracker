73afefc695998f2716617310fac927f8cc361ec7
danielbartsch <daniel.bartsch@adverity.com>
2020-04-09 18:33:39 +0200

    added editable times

--- src/DateRow.tsx
+++ src/DateRow.tsx
@@ -1,11 +1,22 @@
 import * as React from 'react'
-import { isEqual, toString } from './date'
+import { isEqual, toString, fromString } from './date'
 import { holidays, defaultWorkTimes, weekendDays, DayIndex, WorkTime } from './config'
 import { padNumber } from './utils'
 
 const Day = ({ value }: { value: Date }) => <>{toString(value, 'W0. Y4-M2-D2')}</>
 
 const Time = ({ value }: { value: Date }) => <>{toString(value, 'h2:m2')}</>
+
+const TimeInput = ({ value, onChange }: { value: Date; onChange: (date: Date) => void }) => {
+  return (
+    <input
+      type="time"
+      value={toString(value, 'h2:m2')}
+      onChange={event => onChange(fromString(event.target.value, 'h2:m2'))}
+      required
+    />
+  )
+}
 
 type Timestamp = number
 const Duration = ({ value }: { value: Timestamp }) => {
@@ -18,35 +29,54 @@ const Duration = ({ value }: { value: Timestamp }) => {
   )
 }
 
-const WorkTimes = ({ value: [[start, end], ...breaks] }: { value: WorkTime }) => (
-  <>
-    <td>
-      <Time value={start} />
-    </td>
-    <td>
-      <Time value={end} />
-    </td>
-    <td>
-      {breaks.map(([breakStart, breakEnd]) => (
-        <React.Fragment key={`${breakStart.getHours()}-${breakStart.getMinutes()}`}>
-          <Time value={breakStart} />-<Time value={breakEnd} />
-        </React.Fragment>
-      ))}
-    </td>
-    <td>
-      <Duration
-        value={new Date(
-          end.getTime() -
-            start.getTime() -
-            breaks.reduce(
-              (sum, [breakStart, breakEnd]) => sum + (breakEnd.getTime() - breakStart.getTime()),
-              0
-            )
-        ).getTime()}
-      />
-    </td>
-  </>
-)
+const WorkTimes = ({ value }: { value: WorkTime }) => {
+  const [[[start, end], ...breaks], setWorkTime] = React.useState(value)
+  return (
+    <>
+      <td>
+        <TimeInput
+          value={start}
+          onChange={date =>
+            setWorkTime(previousWorkTime => [
+              [date, previousWorkTime[0][1]],
+              ...previousWorkTime.slice(1),
+            ])
+          }
+        />
+      </td>
+      <td>
+        <TimeInput
+          value={end}
+          onChange={date =>
+            setWorkTime(previousWorkTime => [
+              [previousWorkTime[0][0], date],
+              ...previousWorkTime.slice(1),
+            ])
+          }
+        />
+      </td>
+      <td>
+        {breaks.map(([breakStart, breakEnd]) => (
+          <React.Fragment key={`${breakStart.getHours()}-${breakStart.getMinutes()}`}>
+            <Time value={breakStart} />-<Time value={breakEnd} />
+          </React.Fragment>
+        ))}
+      </td>
+      <td>
+        <Duration
+          value={new Date(
+            end.getTime() -
+              start.getTime() -
+              breaks.reduce(
+                (sum, [breakStart, breakEnd]) => sum + (breakEnd.getTime() - breakStart.getTime()),
+                0
+              )
+          ).getTime()}
+        />
+      </td>
+    </>
+  )
+}
 
 const DayStyle = ({
   children,
